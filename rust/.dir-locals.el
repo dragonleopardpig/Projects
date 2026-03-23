630cb5a5a4d0d4080758775a008cc1862f9cfcd6
dragonleopardpig <dragonleopardpig@gmail.com>
2026-03-23 15:35:13 +0800

    fixed

--- rust/.dir-locals.el
+++ rust/.dir-locals.el
@@ -1,4 +1,4 @@
 ;;; Directory Local Variables            -*- no-byte-compile: t -*-
 ;;; lsp-bridge uses this file's location as the project root
 ;;; so rust-analyzer can find Cargo.toml here
-((nil))
+((nil . ((lsp-bridge-user-langserver-dir . "/home/thinky/Projects/rust/lsp-bridge-local"))))
